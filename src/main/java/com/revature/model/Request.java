735eaa7d8174dbf3a358ce8ac9532618597332ea
Saul <sauleff@gmail.com>
2019-06-29 14:37:09 -0400

    Adding POJOs

---
+++ src/main/java/com/revature/model/Request.java
@@ -0,0 +1,108 @@
+package com.revature.model;
+
+import java.util.Date;
+
+public class Request {
+	
+	private int id;
+	private Date timestamp;
+	private double amount;
+	private String description;
+	//private Clob receip;
+	private Users user;
+	private Status status;
+	
+	
+	public Request() {
+		this.timestamp = null;
+		this.amount = 0.0;
+		this.description = "";
+		this.user = null;
+		this.status = null;
+	}
+
+
+	public Request(int id) {
+		this();
+		this.id = id;
+	}
+
+	public Request(int id, Date timestamp, double amount, String description, Users user, Status status) {
+		this.id = id;
+		this.timestamp = timestamp;
+		this.amount = amount;
+		this.description = description;
+		this.user = user;
+		this.status = status;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+
+
+	public double getAmount() {
+		return amount;
+	}
+
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public Users getUser() {
+		return user;
+	}
+
+
+	public void setUser(Users user) {
+		this.user = user;
+	}
+
+
+	public Status getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Request [id=" + id + ", timestamp=" + timestamp + ", amount=" + amount + ", description=" + description
+				+ ", user=" + user + ", status=" + status + "]";
+	}
+	
+	
+
+}
---
+++ src/main/java/com/revature/model/Users.java
@@ -0,0 +1,109 @@
+package com.revature.model;
+
+public class Users {
+	
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String username;
+	private String password;
+	private Role role;
+	
+	
+	
+	public Users() {
+		this.firstName ="";
+		this.lastName ="";
+		this.email = "";
+		this.username ="";
+		this.password = "";
+	}
+	
+	public Users(int id) {
+		this();
+		this.id = id;
+	}
+
+	
+
+	public Users(String username, String password) {
+		this();
+		this.username = username;
+		this.password = password;
+	}
+
+	public Users(int id, String firstName, String lastName, String email, String username, String password, Role role) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.username = username;
+		this.password = password;
+		this.role = role;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "Users [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", username=" + username + ", password=" + password + ", role=" + role + "]";
+	}
+
+
+}
